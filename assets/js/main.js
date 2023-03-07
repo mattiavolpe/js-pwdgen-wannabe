4679cd857e5b8c8778d36358df0eae19ed588a1b
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-03-07 13:57:00 +0100

    ADD JS pseudo-code

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+console.log(`PROVA LINK FILE JS`);
+
+/*
+
+Decrizione passaggi:
+Chiedi all'utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23
+
+*/
+
+// Chiedo all'utente il suo nome e lo salvo in una costante
+// Chiedo all'utente il suo cognome e lo salvo in un'altra costante
+// Chiedo all'utente il suo colore preferito e lo salvo in un'altra costante
+// Creo un elemento nella DOM, all'interno del file HTML, e lo seleziono tramite ID, assegnandolo contemporaneamente ad una costante
+// Assegno alla proprietà innerHTML dell'elemento precedentemente selezionato la concatenazione delle tre costanti contenenti gli input dell'utente e la stringa "23"
